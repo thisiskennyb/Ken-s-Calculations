2a2fcfee50fe0c7f226f05fcf3012f97a7e2cdac
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2023-05-14 17:58:43 -0400

    Functional calculator with flexbox

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let display = document.querySelector("#calculator-display");
+let equationDisplay = document.querySelector("#equation-display")
 
 document.querySelector("#button-1").addEventListener("click", dispValue)
 
@@ -6,15 +7,35 @@ document.querySelector("#button-2").addEventListener("click", dispValue)
 
 document.querySelector("#button-3").addEventListener("click", dispValue)
 
+document.querySelector("#button-4").addEventListener("click", dispValue)
+
+document.querySelector("#button-5").addEventListener("click", dispValue)
+
+document.querySelector("#button-6").addEventListener("click", dispValue)
+
+document.querySelector("#button-7").addEventListener("click", dispValue)
+
+document.querySelector("#button-8").addEventListener("click", dispValue)
+
+document.querySelector("#button-9").addEventListener("click", dispValue)
+
+document.querySelector("#minus").addEventListener("click", minus)
+
 document.querySelector("#plus").addEventListener("click", plus)
 
-// document.querySelector("#equal").addEventListener("click", equals)
+document.querySelector("#times").addEventListener("click", times)
+
+document.querySelector("#divide").addEventListener("click", divide)
+
+document.querySelector("#equal").addEventListener("click", equals)
+
+document.querySelector("#clear").addEventListener("click", clearScreen)
 
 
 
 display.textContent = ''
-
-let total = []
+equationDisplay.textContent = ''
+let total = ''
 
 let operatorClickedLast = false
 
@@ -23,8 +44,9 @@ let operatorClickedLast = false
 
 function dispValue() {
 
-    if (display.textContent.length < 12) {
+    if (display.textContent.length < 15) {
         display.textContent += this.textContent
+        total += this.textContent
     }  
     if (operatorClickedLast == true) {
         display.textContent = '';
@@ -35,11 +57,37 @@ function dispValue() {
 }
 
 
-
 function plus() {
-    total.push(display.textContent)
-    total.push('+')
-    //display.textContent += this.textContent;
+    total += '+'
     operatorClickedLast = true;
+}
+
+function minus() {
+  total += '-'
+  operatorClickedLast = true;
+}
+
+function times() {
+  total += '*'
+  operatorClickedLast = true
+}
+
+function divide() {
+  total += '/'
+  operatorClickedLast = true
+}
+
+function equals() {
+    display.textContent = eval(total)
+    equationDisplay.textContent = total
+    
+}
+
+function clearScreen() {
+  if (display.textContent.length > 0) {
+    display.textContent = ''
+    equationDisplay.textContent = ''
+    total = ''
+  }
 }
     
